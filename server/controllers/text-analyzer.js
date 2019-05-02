d0a93117dafd75f4de2a2d26ffe84e48fa9b1c59
Andre Suchitra <89.andre@gmail.com>
2019-05-02 22:07:15 +0700

    text-analyze frontend

--- server/controllers/text-analyzer.js
+++ server/controllers/text-analyzer.js
@@ -16,15 +16,6 @@ class TextAnalyzer {
         // Detects entities in the document
         const [result] = await client.analyzeEntities({document});
         const entities = result.entities;
-        
-        /* console.log('Entities:');
-        entities.forEach(entity => {
-            console.log(entity.name);
-            console.log(` - Type: ${entity.type}, Salience: ${entity.salience}`);
-            if (entity.metadata && entity.metadata.wikipedia_url) {
-                console.log(` - Wikipedia URL: ${entity.metadata.wikipedia_url}$`);
-            }
-        }); */
 
         if(entities) {
             res.status(200).json(entities)
