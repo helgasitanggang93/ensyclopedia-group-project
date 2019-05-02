f301d3d0ff4e52bfde2bc1090a3c1d9a3d2b7e12
Andre Suchitra <89.andre@gmail.com>
2019-05-02 21:10:41 +0700

    integrate google text analysis

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,11 @@
 const router = require('express').Router()
 const authenticate = require('../middlewares/authenticate')
 const userController = require('../controllers/user-controller')
+const TextAnalyzer = require('../controllers/text-analyzer')
 
 router.post('/signup', userController.register)
 router.post('/signin', userController.login)
+
+router.post('/analyze', TextAnalyzer.analyze)
 
 module.exports = router
