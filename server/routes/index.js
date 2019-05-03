18d5b1d8a573d3866699ea1393a00b9635e233ee
dinulsyah <arif.dinulsyah.p@mail.ugm.ac.id>
2019-05-03 15:21:10 +0700

    google-news

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,11 +3,12 @@ const authenticate = require('../middlewares/authenticate')
 const userController = require('../controllers/user-controller')
 const TextAnalyzer = require('../controllers/text-analyzer')
 const Youtube = require('../controllers/youtube')
+const googleNews = require('../controllers/google-news')
 
 router.post('/signup', userController.register)
 router.post('/signin', userController.login)
 router.post('/gSignIn', userController.googleSign)
-
+router.get('/google-news/:title',googleNews.fetch)
 router.post('/analyze', TextAnalyzer.analyze)
 router.get('/youtube', Youtube.show)
 
